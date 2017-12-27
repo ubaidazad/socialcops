c0eff5fadd3890a12cef71b78682a463eb3f0b5f
Ubaid Azad <ubaidazad@outlook.com>
2017-12-27 11:47:52 +0530

    added docker support.

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import * as bodyParser from "body-parser";
 import * as express from "express";
 import * as morgan from "morgan";
 import { apiRoutes } from "./routes/routes";
+import * as mkdirp from 'mkdirp';
 
 const app = express();
-const port = process.env.port || 3000;
+const port = 3000;
+const host = '0.0.0.0';
 
 // configure app to get info from URL's
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,13 +14,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // configure logging
-// app.use(morgan('dev'));
+app.use(morgan('combined'));
 
 // configure routes
 app.use("/api", apiRoutes);
 
+// create required directories
+mkdirp('images', () => { });
+mkdirp('thumbnails', () => { })
+
 // listen server on port
-const server = app.listen(port, () => console.log(`listening on port ${port}!`));
+const server = app.listen(port, host);
+console.log(`app listening on port ${host}:${port}!`);
 
 module.exports = {
     server,
